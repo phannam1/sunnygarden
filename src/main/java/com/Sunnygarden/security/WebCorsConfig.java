f5df3eea4599c5e31bf685d0d7a8af462277db1e
BANVIENCORP\nam.phan-thanh <nam.phan-thanh@banvien.com.vn>
2023-05-12 16:35:28 +0700

    commit filter cors

---
+++ src/main/java/com/Sunnygarden/security/WebCorsConfig.java
@@ -0,0 +1,23 @@
+package com.Sunnygarden.security;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@AllArgsConstructor
+public class WebCorsConfig {
+
+@Bean
+public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurerAdapter() {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**").allowedOrigins("*");
+        }
+    };
+}
+}
