b7f5dd8a0bb16cbb5ee1125ffe9a8f9ad16ff790
phannam1 <35187952+phannam1@users.noreply.github.com>
2023-06-04 10:48:11 +0700

    commit final

--- src/main/java/com/Sunnygarden/common/Constants.java
+++ src/main/java/com/Sunnygarden/common/Constants.java
@@ -2,5 +2,5 @@ package com.Sunnygarden.common;
 
 public class Constants {
 	public static String EMAIL_FROM = "page.sg.us.info@gmail.com";
-	public static String EMAIL_TO = "troidem0952@gmail.com";
+	public static String EMAIL_TO = "corolla16@hotmail.com";
 }
